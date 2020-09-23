a435b76b533c60e6477208d6446967f382a79ce9
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-23 12:24:07 +0700

    🎉 start the lesson

--- components/tests/HelloCube.vue
+++ components/tests/HelloCube.vue
@@ -1,5 +1,12 @@
 <template>
-	<div></div>
+<div id="sceneSpace" class="w-screen h-screen">
+	<div class="absolute flex justify-center w-screen text-gray-500">
+		<div class="text-center">
+			<h1>Example</h1>
+			<p>Source: <a target="_blank" href="https://threejsfundamentals.org/threejs/lessons/threejs-fundamentals.html"><span class="text-indigo-500">Threejs Fundamentals</span></a></p>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
