7c847aec366b5150dce83ab9f18e495ee34c064f
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-28 00:40:20 +0700

    move files

--- pages/tours/_id.vue
+++ pages/tours/_id.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="bg-gray-100">
-		<Building v-if="$route.params.id == 'office_building'"/>
-		<Interior v-if="$route.params.id == 'office_interior'"/>
-		<SwampLocation v-if="$route.params.id == 'swamplocation'"/>
-		<Castle v-if="$route.params.id == 'castle'"/>
-		<Classroom v-if="$route.params.id == 'classroom'"/>
-		<Gym v-if="$route.params.id == 'gym'"/>
-		<GammingRoom v-if="$route.params.id == 'gameroom'"/>
-  </div>
+<div class="bg-gray-100">
+	<Building v-if="$route.params.id == 'office_building'"/>
+	<Interior v-if="$route.params.id == 'office_interior'"/>
+	<SwampLocation v-if="$route.params.id == 'swamplocation'"/>
+	<Castle v-if="$route.params.id == 'castle'"/>
+	<Classroom v-if="$route.params.id == 'classroom'"/>
+	<Gym v-if="$route.params.id == 'gym'"/>
+	<GammingRoom v-if="$route.params.id == 'gameroom'"/>
+	<h1 v-else>Hahahahaha</h1>
+</div>
 </template>
 
 <script>
