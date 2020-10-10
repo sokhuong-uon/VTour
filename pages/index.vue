83e708598674d90a442b4db4e75f4c816ce50b71
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-10-10 17:28:03 +0700

    remove redundant items and add a visit link to the origin source

--- pages/index.vue
+++ pages/index.vue
@@ -56,32 +56,13 @@
 					</div>
 				</div>
 			</div>
-			<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4 h-72">
-				<div class="w-full h-full p-2">
-					<div class="w-full h-full overflow-hidden shadow hover:shadow-xl">
-						<nuxt-link class="w-full h-full" to="/tours/gameroom">
-							<img class="w-full h-full object-cover transform transition-all duration-500 hover:scale-125" src="img/gameroom.jpg" alt="">
-						</nuxt-link>
-					</div>
-				</div>
-			</div>
 
 			<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4 h-72">
 				<div class="w-full h-full p-2">
-					<div class="w-full h-full overflow-hidden shadow hover:shadow-xl">
-						<nuxt-link class="w-full h-full" to="/tours/swamplocation">
-							<img class="w-full h-full object-cover transform transition-all duration-500 hover:scale-125" src="img/swamplocation.jpg" alt="">
-						</nuxt-link>
-					</div>
-				</div>
-			</div>
-
-			<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4 h-72">
-				<div class="w-full h-full p-2">
-					<div class="w-full h-full overflow-hidden shadow hover:shadow-xl">
-						<nuxt-link class="w-full h-full" to="/tours/test">
-							<!--<img class="w-full h-full object-cover transform transition-all duration-500 hover:scale-125" src="img/swamplocation.jpg" alt="">-->
-							<iframe src="/tours/test" class="w-full h-full"></iframe>
+					<div class="relative w-full h-full overflow-hidden shadow hover:shadow-xl">
+						<iframe src="/tours/test" class="w-full h-full"></iframe>
+						<nuxt-link class="absolute right-0 top-0 w-12 h-8 rounded-bl-md bg-gray-800 text-gray-200 flex justify-center items-center hover:shadow-lg hover:bg-gray-700" to="/tours/test">
+							<p class="">Visit</p>
 						</nuxt-link>
 					</div>
 				</div>
