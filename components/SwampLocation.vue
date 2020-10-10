64afb8df1a573fda340afc4f3a9eb0389d06b497
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-10-10 15:37:38 +0700

    remove draco loader

--- components/SwampLocation.vue
+++ components/SwampLocation.vue
@@ -20,7 +20,6 @@ import {
 import Stats from '~/node_modules/three/examples/jsm/libs/stats.module.js';
 import {GLTFLoader} from '~/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from '~/node_modules/three/examples/jsm/controls/OrbitControls.js';
-import {DRACOLoader} from '~/node_modules/three/examples/jsm/loaders/DRACOLoader.js';
 
 export default {
 	name: 'SwampLocation',
