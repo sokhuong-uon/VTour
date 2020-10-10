e4fe2e30d3dfbcae48b432c48dce7c564bd2b18a
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-10-10 23:50:37 +0700

    do some camera movement

--- components/Test.vue
+++ components/Test.vue
@@ -1,6 +1,9 @@
 <template>
 	<div id="sceneSpace" class="w-screen h-screen">
-
+		<div class="absolute right-4 top-4 text-gray-200">
+			<button @click="juphBetweenCube(0)" class="w-10 h-8 bg-gray-700">0</button>
+			<button @click="juphBetweenCube(1)" class="w-10 h-8 bg-gray-700">1</button>
+		</div>
 	</div>
 </template>
 
@@ -9,10 +12,12 @@
 
 import {
 	PerspectiveCamera, AmbientLight, Scene, WebGLRenderer,
-	AxesHelper, GridHelper,
-	PlaneBufferGeometry, MeshBasicMaterial, Mesh, BoxBufferGeometry, BackSide,
-	DirectionalLight, MeshLambertMaterial, TextureLoader
+	AxesHelper, GridHelper, Raycaster, Vector2,
+	PlaneBufferGeometry, MeshBasicMaterial, Mesh, BoxBufferGeometry, BackSide, DoubleSide,
+	DirectionalLight, MeshLambertMaterial, TextureLoader, CubeTextureLoader, CircleBufferGeometry,
+	CylinderBufferGeometry, SphereBufferGeometry
 } from 'three';
+import gsap from 'gsap';
 import Stats from '~/node_modules/three/examples/jsm/libs/stats.module.js';
 import {GLTFLoader} from '~/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from '~/node_modules/three/examples/jsm/controls/OrbitControls.js';
@@ -27,6 +32,10 @@ export default {
 			renderer: null,
 			stats: null,
 			controls: null,
+			circleOverHelper: null,
+			raycaster: null,
+			mouse: null,
+			targetObjects: [],
 
 			plane: null,
 			cubes: []
@@ -69,7 +78,7 @@ export default {
 				const far = 1000;
 				this.camera = new PerspectiveCamera(fov, aspect, near, far);
 				this.camera.name = "Camera";
-				this.camera.position.set(0.5, 2, 0.5);
+				this.camera.position.set(0, 4, 4);
 				// this.camera.rotation.y = -Math.PI;
 			}
 
@@ -93,13 +102,13 @@ export default {
 			}
 
 			// Orbit Controls
-			{
-				this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-				this.controls.target.set(0,0.5,0);
-				this.controls.minDistance = 0.5;
-				this.controls.maxDistance = 100;
-				this.controls.update();
-			}
+			// {
+			// 	this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+			// 	this.controls.target.set(0,0,0);
+			// 	this.controls.minDistance = 0.5;
+			// 	this.controls.maxDistance = 100;
+			// 	this.controls.update();
+			// }
 
 			// Axes Helper
 			{
@@ -115,36 +124,84 @@ export default {
 				this.scene.add( gridHelper );
 			}
 
-			// Plane
+			// Circle Helper
 			// {
-			// 	const geometry = new PlaneBufferGeometry(10, 10);
-			// 	const material = new MeshBasicMaterial({transparent: true});
-			// 	this.plane = new Mesh(geometry, material);
-			// 	this.plane.rotateX(-Math.PI / 2);
-			// 	this.scene.add(this.plane);
+			// 	let geometry = new CylinderBufferGeometry(0.3, 0.3, 0.01, 60);
+			// 	let material = new MeshBasicMaterial({color: 0x353535});
+			// 	this.circleOverHelper = new Mesh(geometry, material);
+			// 	this.circleOverHelper.rotateX(-Math.PI / 2);
+			// 	const outline = new Mesh(
+			// 		new CylinderBufferGeometry(0.2, 0.2, 0.02, 60),
+			// 		new MeshBasicMaterial({color: 0xfffffa, side: DoubleSide})
+			// 	);
+			// 	// outline.rotateX(-Math.PI/2);
+			// 	this.circleOverHelper.add(outline);
+			// 	this.circleOverHelper.rotateX(-Math.PI/2);
+			// 	this.scene.add(this.circleOverHelper);
 			// }
+
+			// Plane
+			{
+				const geometry = new PlaneBufferGeometry(10, 10);
+				const material = new MeshBasicMaterial({color: 0x233568, opacity: 0.001, transparent: true});
+				this.plane = new Mesh(geometry, material);
+				this.plane.name = "Plane";
+				this.plane.rotateX(-Math.PI / 2);
+				this.plane.position.set(0,0.0000001,0)
+				this.scene.add(this.plane);
+			}
 
 			// Cube
 			{
-				const geometry = new BoxBufferGeometry(1, 1, 1);
-				const material = new MeshLambertMaterial({color: 0x353535, side: BackSide});
-				const loader = new TextureLoader();
-				const materials = [
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=128&h=128&q=100'), side: BackSide}),
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&q=100'), side: BackSide}),
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&q=100'), side: BackSide}),
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&q=100'), side: BackSide}),
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&q=100'), side: BackSide}),
-					new MeshBasicMaterial({map: loader.load('https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=128&h=128&q=100'), side: BackSide}),
-				];
-				for(let i=-4.5; i<5; i++){
-					for(let j=-4.5; j<5; j++){
-						let cube = new Mesh(geometry, materials);
-						cube.position.set(i, 0.5, j);
-						this.scene.add(cube);
-					}
-				}
+				const geometry = new BoxBufferGeometry(2, 2, 2);
+				// const geometry = new SphereBufferGeometry(2, 64, 64);
+				// const material = new MeshLambertMaterial({color: 0x353535, side: BackSide});
+				// const loader = new TextureLoader();
+				// const materials = [
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/px.png'), side: BackSide}),
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/nx.png'), side: BackSide}),
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/py.png'), side: BackSide}),
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/ny.png'), side: BackSide}),
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/pz.png'), side: BackSide}),
+				// 	new MeshBasicMaterial({map: loader.load('/cubemaps/group1/nz.png'), side: BackSide}),
+				// ];
+				const loader = new CubeTextureLoader();
+				loader.setPath( '/cubemaps/group1/' );
+
+				let textureCube = loader.load( [
+					'px.png',
+					'nx.png',
+					'py.png',
+					'ny.png',
+					'pz.png',
+					'nz.png'
+				] );
+
+				let material = new MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, side: BackSide} );
+				// for(let i=-4; i<=4; i+=2){
+				// 	for(let j=-4; j<=4; j+=2){
+				// 		let cube = new Mesh(geometry, materials);
+				// 		cube.position.set(i, 1, j);
+				// 		this.scene.add(cube);
+				// 		this.targetObjects.push(cube);
+				// 	}
+				// }
+				let cube = new Mesh(geometry, material);
+				cube.position.set(0, 1, 0);
+				this.scene.add(cube);
+				this.targetObjects.push(cube);
+				this.renderer.background = material;
+
 			}
+
+			this.targetObjects.push(this.plane);
+
+
+			// Raycaster
+			this.raycaster = new Raycaster();
+
+			// Mouse
+			this.mouse = new Vector2();
 
 			this.renderer.setAnimationLoop(this.animate);
 		},
@@ -156,6 +213,43 @@ export default {
 			}, 1000 / 60);	// 60 fps, 1000/30 for 30 fps
 		},
 
+		initControls() {
+			this.camera.rotation.order = "YXZ";
+
+			// Mouse down
+			this.sceneSpace.addEventListener("mousedown", (evt) => {
+				this.mouseDown = true;
+				this.sceneSpace.style.cursor = "grab";
+			}, false);
+
+			// Mouse up
+			this.sceneSpace.addEventListener("mouseup", (evt) => {
+				this.sceneSpace.style.cursor = "default";
+				this.mouseDown = false;
+			}, false);
+
+			this.sceneSpace.addEventListener("mousemove", (evt) => {
+				let movementX = evt.movementX || evt.mozMovementX || evt.webkitMovementX || 0;
+				let movementY = evt.movementY || evt.mozMovementY || evt.webkitMovementY || 0;
+
+				if (this.mouseDown) {
+					this.sceneSpace.style.cursor = "grabbing";
+					this.camera.rotation.y -= -movementX / 600;
+					this.camera.rotation.x -= -movementY / 600;
+				}
+			},false);
+		},
+
+		juphBetweenCube(cubeID){
+			gsap.to(this.camera.position,{
+				x: 0,
+				y: 1,
+				z: cubeID,
+				ease: "Power3.InOut",
+				duration: 2,
+			})
+		},
+
 		onWindowResize() {
 			this.renderer.setSize(this.sceneSpace.clientWidth, this.sceneSpace.clientHeight);
 			this.camera.aspect = this.sceneSpace.clientWidth / this.sceneSpace.clientHeight;
@@ -163,8 +257,11 @@ export default {
 		},
 	},
 
+
+
 	mounted() {
 		this.init();
+		this.initControls();
 		window.addEventListener('resize', this.onWindowResize, false);
 	},
 
