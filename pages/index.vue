38bfc2c51312472e3df1e9bc4e4c3a4ef20ecfd3
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-11-04 18:51:36 +0700

    🔨 change order of iframes

--- pages/index.vue
+++ pages/index.vue
@@ -50,10 +50,7 @@
 			<div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 h-90">
 				<div class="w-full h-full p-2">
 					<div class="relative w-full h-full overflow-hidden shadow hover:shadow-xl">
-						<iframe id="360_180" src="/tours/360_180" class="w-full h-full"></iframe>
-						<button @click="openFullscreen('360_180')" class="absolute right-0 top-0 w-8 h-8 bg-green-800 text-gray-100">
-							full
-						</button>
+						<iframe class="w-full h-full" style="width: 100%; height: 100%;" src="https://nokor.netlify.app/tours/a/" frameborder="0" allowfullscreen allow="vr; xr-spatial-tracking"></iframe>
 					</div>
 				</div>
 			</div>
@@ -61,10 +58,14 @@
 			<div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 h-90">
 				<div class="w-full h-full p-2">
 					<div class="relative w-full h-full overflow-hidden shadow hover:shadow-xl">
-						<iframe class="w-full h-full" style="width: 100%; height: 100%;" src="https://nokor.netlify.app/tours/a/" frameborder="0" allowfullscreen allow="vr; xr-spatial-tracking"></iframe>
+						<iframe id="360_180" src="/tours/360_180" class="w-full h-full"></iframe>
+						<button @click="openFullscreen('360_180')" class="absolute right-0 top-0 w-8 h-8 bg-green-800 text-gray-100">
+							full
+						</button>
 					</div>
 				</div>
 			</div>
+			
 		</div>
 	</div>
 </div>
